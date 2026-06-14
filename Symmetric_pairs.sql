9f927b557c12fb4e0ceddb2da45a435e06f84237
Krishita Singh <krishitaps999@gmail.com>
2026-06-14 21:51:08 +0530

    Add query to find symmetric pairs in Functions table
    Query retrieves symmetric pairs from the Functions table, ensuring pairs are listed in ascending order by X and that X1 ≤ Y1.

---
+++ Symmetric_pairs.sql
@@ -0,0 +1,20 @@
+-- Task : You are given a table, Functions, containing two columns: X and Y.
+
+-- Two pairs (X1, Y1) and (X2, Y2) are said to be symmetric pairs if X1 = Y2 and X2 = Y1.
+
+-- Write a query to output all such symmetric pairs in ascending order by the value of X. List the rows such that X1 ≤ Y1.
+
+SELECT X, Y 
+FROM Functions 
+WHERE X = Y 
+GROUP BY X, Y 
+HAVING COUNT(*) > 1
+
+UNION
+
+SELECT f1.X, f1.Y 
+FROM Functions f1
+JOIN Functions f2 ON f1.X = f2.Y AND f1.Y = f2.X
+WHERE f1.X < f1.Y
+
+ORDER BY X;
