1bceac1c8d78889b226b191a9cec5ee6987605dc
Krishita Singh <krishitaps999@gmail.com>
2026-06-16 15:03:22 +0530

    Enhance symmetric pairs query with duplicates
    Added a second query to find symmetric pairs, including duplicates.

--- Symmetric_pairs.sql
+++ Symmetric_pairs.sql
@@ -4,6 +4,23 @@
 
 -- Write a query to output all such symmetric pairs in ascending order by the value of X. List the rows such that X1 ≤ Y1.
 
+-- #1
+SELECT X, Y 
+FROM Functions 
+WHERE X = Y 
+GROUP BY X, Y 
+HAVING COUNT(*) > 1
+
+UNION
+
+SELECT f1.X, f1.Y 
+FROM Functions f1
+JOIN Functions f2 ON f1.X = f2.Y AND f1.Y = f2.X
+WHERE f1.X < f1.Y
+
+ORDER BY X;
+
+-- #2
 SELECT X, Y 
 FROM Functions 
 WHERE X = Y 
