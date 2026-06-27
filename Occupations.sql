291e59a4393c3ebe399e1c88da2804294f094ec3
Krishita Singh <krishitaps999@gmail.com>
2026-06-27 21:04:18 +0530

    Update Occupations.sql

--- Occupations.sql
+++ Occupations.sql
@@ -1,6 +1,6 @@
-Task : Pivot the Occupation column in OCCUPATIONS so that each Name is sorted alphabetically and displayed underneath its corresponding Occupation. The output should consist of four columns (Doctor, Professor, Singer, and Actor) in that specific order, with their respective names listed alphabetically under each column.
+-- Task : Pivot the Occupation column in OCCUPATIONS so that each Name is sorted alphabetically and displayed underneath its corresponding Occupation. The output should consist of four columns (Doctor, Professor, Singer, and Actor) in that specific order, with their respective names listed alphabetically under each column.
 
-Note: Print NULL when there are no more names corresponding to an occupation.
+-- Note: Print NULL when there are no more names corresponding to an occupation.
 
 SELECT 
     MAX(CASE WHEN Occupation = 'Doctor' THEN Name END) AS Doctor,
